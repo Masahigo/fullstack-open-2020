e9d23c8f246119a861874575c70e09a18fceb505
Masi Malmi <masi.malmi@polarsquad.com>
2020-11-03 11:06:28 +0200

    Part 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name}</p>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    //console.log('button clicked', event.target)
+    const personObject = {
+      name: newName
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: 
+          <input 
+            value={newName} 
+            onChange={handleNameChange} 
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug (persons): {persons.map(person => person.name + ', ')}</div>
+      <div>debug (newName): {newName}</div>
+      {persons.map(person => 
+          <Person key={person.name} person={person} />
+      )}
+    </div>
+  )
+
+}
+
+export default App
