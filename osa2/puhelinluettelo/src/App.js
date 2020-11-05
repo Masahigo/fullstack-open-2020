c11fd9b1e7e55aced8783f0f5c40ba92459d3db9
Masi Malmi <masi.malmi@polarsquad.com>
2020-11-05 07:22:33 +0200

    Part 2.15

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -81,12 +81,18 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-    
-      setPersons(persons.concat(personObject))
-    }
 
-    setNewName('')
-    setNewNumber('')
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          //console.log(response)
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+      })
+    
+    }
+    
   }
 
   const handleNameChange = (event) => {
