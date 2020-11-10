b2437df0b1aefe4cc072346be300b60575c70beb
Masi Malmi <masahigo@gmail.com>
2020-11-10 12:01:11 +0200

    Part 3.8

--- osa3/index.js
+++ osa3/index.js
@@ -3,7 +3,15 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(
+    //morgan('tiny')
+    morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
+
+morgan.token('body', function (req, res) { 
+    //console.log('morgan token (req.body)', req.body)
+    return JSON.stringify(req.body)
+})
 
 /*
 const requestLogger = (request, response, next) => {
