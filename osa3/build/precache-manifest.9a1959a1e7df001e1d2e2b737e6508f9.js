9137b014fff45b39af2526513d8cfb054121d708
Masi Malmi <masahigo@gmail.com>
2020-11-11 16:49:42 +0200

    uibuild

---
+++ osa3/build/precache-manifest.9a1959a1e7df001e1d2e2b737e6508f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce72a42698a083adad3890ef72c457b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6397f3caf79554596ff6",
+    "url": "/static/css/main.efc48fce.chunk.css"
+  },
+  {
+    "revision": "06519ba7d7a08b536886",
+    "url": "/static/js/2.040d948e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.040d948e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6397f3caf79554596ff6",
+    "url": "/static/js/main.c5bfd582.chunk.js"
+  },
+  {
+    "revision": "0df48ebd09e3ec15b5eb",
+    "url": "/static/js/runtime-main.88092800.js"
+  }
+]);
