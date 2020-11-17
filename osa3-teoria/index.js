59aca026a2a621437fbff7d3a6cac6be7b188ff0
Masi Malmi <masahigo@gmail.com>
2020-11-17 11:06:53 +0200

    Testing ESLint.

--- osa3-teoria/index.js
+++ osa3-teoria/index.js
@@ -8,11 +8,11 @@ app.use(cors())
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
 
 app.use(requestLogger)
@@ -57,31 +57,31 @@ app.get('/', (req, res) => {
 */
 
 app.get('/api/notes', (req, res, next) => {
-    //res.json(notes)
-    Note.find({}).then(notes => {
-        res.json(notes.map(note => note.toJSON()))
-    })
-        .catch(error => next(error))
+  //res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes.map(note => note.toJSON()))
+  })
+    .catch(error => next(error))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
 
-    // https://mongoosejs.com/docs/api.html#model_Model.findById
-    Note.findById(request.params.id).then(note => {
-        //response.json(note)
-        if (note) {
-            response.json(note.toJSON())
-        } else {
-            response.status(404).end()
-        }
+  // https://mongoosejs.com/docs/api.html#model_Model.findById
+  Note.findById(request.params.id).then(note => {
+    //response.json(note)
+    if (note) {
+      response.json(note.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => {
+      //console.log(error)
+      //response.status(400).send({ error: 'malformatted id' })
+      next(error)
     })
-        .catch(error => {
-            //console.log(error)
-            //response.status(400).send({ error: 'malformatted id' })
-            next(error)
-        })
 
-    /*
+  /*
     const id = Number(request.params.id)
     console.log(id)
     const note = notes.find(note => {
@@ -96,47 +96,49 @@ app.get('/api/notes/:id', (request, response, next) => {
     }
     */
 
-    //console.log(note)
-    //response.json(note)
+  //console.log(note)
+  //response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-    /*const id = Number(request.params.id)
+  /*const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
 
     response.status(204).end()*/
 
-    // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
-    Note.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 
 })
 
+/*
 const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
 }
+*/
 
 app.post('/api/notes', (request, response, next) => {
-    //console.log(request.headers)
-    //const note = request.body
-    //console.log(note)
+  //console.log(request.headers)
+  //const note = request.body
+  //console.log(note)
 
-    const body = request.body
+  const body = request.body
 
-    if (!body.content) {
-        // HUOM: return
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
+  if (!body.content) {
+    // HUOM: return
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
 
-    /*const note = {
+  /*const note = {
         content: body.content,
         important: body.important || false,
         date: new Date(),
@@ -148,54 +150,54 @@ app.post('/api/notes', (request, response, next) => {
     response.json(note)
     */
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
 
-    note.save()
-        .then(savedNote => savedNote.toJSON())
-        .then(savedAndFormattedNote => {
-            response.json(savedAndFormattedNote)
-        })
-        .catch(error => next(error))
+  note.save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
 
-    // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
-    // by default would return the document as it was before update was applied
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-        .then(updatedNote => {
-            response.json(updatedNote.toJSON())
-        })
-        .catch(error => next(error))
+  // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
+  // by default would return the document as it was before update was applied
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 // olemattomien osoitteiden käsittely
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 // virheellisten pyyntöjen käsittely
@@ -203,7 +205,7 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 //console.log(`Server running on port ${port}`)
