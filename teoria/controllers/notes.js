a3f2199fcb8f87ac6fdcffcf9eb909c027ba55b6
Masi Malmi <masahigo@gmail.com>
2020-11-27 11:01:56 +0200

    Part 4.1 theory

---
+++ teoria/controllers/notes.js
@@ -0,0 +1,166 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+/*
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2020-01-10T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2020-01-10T18:39:34.091Z",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2020-01-10T19:20:14.298Z",
+        important: true
+    }
+]
+
+const app = http.createServer((request, response) => {
+response.writeHead(200, { 'Content-Type': 'application/json' })
+response.end(JSON.stringify(notes))
+})
+
+const port = 3001
+app.listen(port)
+*/
+
+/*
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+*/
+
+notesRouter.get('/', (req, res, next) => {
+  //res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes.map(note => note.toJSON()))
+  })
+    .catch(error => next(error))
+})
+
+notesRouter.get('/:id', (request, response, next) => {
+
+  // https://mongoosejs.com/docs/api.html#model_Model.findById
+  Note.findById(request.params.id).then(note => {
+    //response.json(note)
+    if (note) {
+      response.json(note.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => {
+      //logger.info(error)
+      //response.status(400).send({ error: 'malformatted id' })
+      next(error)
+    })
+
+  /*
+    const id = Number(request.params.id)
+    logger.info(id)
+    const note = notes.find(note => {
+        //logger.info(note.id, typeof note.id, id, typeof id, note.id === id)
+        return note.id === id
+    })
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+    */
+
+  //logger.info(note)
+  //response.json(note)
+})
+
+notesRouter.delete('/:id', (request, response, next) => {
+  /*const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()*/
+
+  // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+
+})
+
+/*
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+*/
+
+notesRouter.post('/', (request, response, next) => {
+  //logger.info(request.headers)
+  //const note = request.body
+  //logger.info(note)
+
+  const body = request.body
+
+  if (!body.content) {
+    // HUOM: return
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  /*const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+    */
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note.save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote)
+    })
+    .catch(error => next(error))
+})
+
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
+  // by default would return the document as it was before update was applied
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+module.exports = notesRouter
