fc650f0b2db41b818605e7cc8c16f702e02860b0
Masi Malmi <masahigo@gmail.com>
2020-11-30 10:18:39 +0200

    Include config

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,13 +1,24 @@
-require('dotenv').config();
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
 
+const mongoose = require('mongoose')
+
 const Blog = require('./models/blog')
+
+logger.info('connecting to', config.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB:', error.message);
+  });
 
 app.use(cors())
 app.use(express.json())
@@ -20,7 +31,6 @@ app.use(middleware.unknownEndpoint)
 // virheellisten pyyntöjen käsittely
 app.use(middleware.errorHandler)
 
-const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const logger = require('../utils/logger')
 
 const blogSchema = mongoose.Schema({
     title: String,
@@ -15,16 +14,5 @@ blogSchema.set('toJSON', {
       delete returnedObject.__v;
     },
   });
-
-const mongoUrl = process.env.MONGODB_URI;
-
-logger.info('connecting to', mongoUrl);
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => {
-        logger.info('connected to MongoDB');
-    })
-    .catch((error) => {
-        logger.info('error connecting to MongoDB:', error.message);
-    });
 
 module.exports = mongoose.model('Blog', blogSchema);
