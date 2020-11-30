c61da166b57191d14ee1bc5ed3b885d643d2911b
Masi Malmi <masahigo@gmail.com>
2020-11-30 11:15:40 +0200

    Part 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,23 @@
+const logger = require('./logger')
+
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        //logger.info('sum', sum)
+        //logger.info('item', item)
+        return sum + item
+    }
+
+    return blogs.length === 0
+    ? 0 
+    : blogs.map(blog => blog.likes).reduce(reducer, 0)
+}
+
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }
