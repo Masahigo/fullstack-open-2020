39a3f32a9c295c503ebd889f78d5e2cdf3832c95
Masi Malmi <masahigo@gmail.com>
2020-11-30 10:22:16 +0200

    Wrap up part 4.2.

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,36 +1,10 @@
+const app = require('./app') // varsinainen Express-sovellus
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const mongoose = require('mongoose')
+const server = http.createServer(app)
 
-const Blog = require('./models/blog')
-
-logger.info('connecting to', config.MONGODB_URI);
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    logger.info('connected to MongoDB');
-  })
-  .catch((error) => {
-    logger.info('error connecting to MongoDB:', error.message);
-  });
-
-app.use(cors())
-app.use(express.json())
-
-app.use(middleware.requestLogger)
-app.use('/api/blogs', blogsRouter)
-
-// olemattomien osoitteiden käsittely
-app.use(middleware.unknownEndpoint)
-// virheellisten pyyntöjen käsittely
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
