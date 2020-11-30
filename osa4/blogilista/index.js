8dda1998ff02dbfe79e234d94d5027096ead4ec7
Masi Malmi <masahigo@gmail.com>
2020-11-30 10:13:35 +0200

    Include logger and middleware.

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -4,15 +4,23 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
 const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
 
+app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
+
+// olemattomien osoitteiden käsittely
+app.use(middleware.unknownEndpoint)
+// virheellisten pyyntöjen käsittely
+app.use(middleware.errorHandler)
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const logger = require('../utils/logger')
 
 const blogSchema = mongoose.Schema({
     title: String,
@@ -17,13 +18,13 @@ blogSchema.set('toJSON', {
 
 const mongoUrl = process.env.MONGODB_URI;
 
-console.log('connecting to', mongoUrl);
+logger.info('connecting to', mongoUrl);
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(() => {
-        console.log('connected to MongoDB');
+        logger.info('connected to MongoDB');
     })
     .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message);
+        logger.info('error connecting to MongoDB:', error.message);
     });
 
 module.exports = mongoose.model('Blog', blogSchema);
