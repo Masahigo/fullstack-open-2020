46df43cdbf464b23c386d2c22a38466b7b7bbabb
Masi Malmi <masahigo@gmail.com>
2020-12-01 15:09:04 +0200

    Finish Part 4b theory.

--- teoria/tests/note_api.test.js
+++ teoria/tests/note_api.test.js
@@ -8,10 +8,34 @@ const Note = require('../models/note')
 
 beforeEach(async () => {
   await Note.deleteMany({})
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  
+  /*console.log('cleared')
+
+  helper.initialNotes.forEach(async (note) => {
+    let noteObject = new Note(note)
+    await noteObject.save()
+    console.log('saved')
+  })
+  console.log('done')*/
+
+  
+
+  /* VER1 - promises are executed in parallel, in no specific order
+
+  const noteObjects = helper.initialNotes
+  
+    .map(note => new Note(note))
+  const promiseArray = noteObjects.map(note => note.save())
+  await Promise.all(promiseArray)*/
+
+  /* VER2 - execute promises in order
+  for (let note of noteObjects) {
+    let noteObject = new Note(note)
+    await noteObject.save()
+  } */
+
+  // VER3 - skip promises and use ORM's built-in method
+  await Note.insertMany(helper.initialNotes)
 })
 
 test('notes are returned as json', async () => {
