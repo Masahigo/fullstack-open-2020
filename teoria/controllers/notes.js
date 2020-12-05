d1b25cc65251c90ae82c568a21d83bc671fdc83f
Masi Malmi <masahigo@gmail.com>
2020-12-05 17:42:39 +0200

    Part 4c theory: user model and reference.

--- teoria/controllers/notes.js
+++ teoria/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 // try catch magic
 // next removed from params also
@@ -138,6 +139,8 @@ notesRouter.post('/', async (request, response) => {
 
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   if (!body.content) {
     // HUOM: return
     return response.status(400).json({
@@ -149,10 +152,14 @@ notesRouter.post('/', async (request, response) => {
     content: body.content,
     important: body.important === undefined ? false : body.important,
     date: new Date(),
+    user: user._id
   })
 
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+  
   response.json(savedNote.toJSON())
 
   
