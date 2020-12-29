ff245934d8388f9dd2c1abde5884ec14bad499ff
Masi Malmi <masahigo@gmail.com>
2020-12-29 19:27:44 +0200

    Part 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,44 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailView, setDetailView] = useState(false)
+
+  const hideWhenDetailView = { display: detailView ? 'none' : '' }
+  const showWhenDetailView = { display: detailView ? '' : 'none' }
+
+  const toggleDetailView = () => {
+    setDetailView(!detailView)
+  }
+
+  const addLike = (event) => {
+    event.preventDefault()
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenDetailView}>
+        {blog.title} &nbsp;
+        <button onClick={toggleDetailView}>view</button>
+      </div>
+      <div style={showWhenDetailView}>
+        {blog.title} &nbsp;
+        <button onClick={toggleDetailView}>hide</button><br />
+        {blog.url}<br />
+        likes {blog.likes}&nbsp; <button onClick={addLike}>like</button><br />
+        {blog.author}
+      </div>
+
+    </div>
+  )
+}
 
 export default Blog
