c28b577f66f3e48a5da31e460465442af9c5ed2b
Masi Malmi <masahigo@gmail.com>
2020-12-30 16:41:29 +0200

    Part 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders blog\'s title in list view', () => {
@@ -18,5 +18,40 @@ test('renders blog\'s title in list view', () => {
     const div = component.container.querySelector('.list-view')
     expect(div).toHaveTextContent(
         'The best posts about DevOps'
+    )
+})
+
+test('renders all details of a blog after view button clicked', () => {
+    const blog = {
+        title: 'The best posts about DevOps',
+        author: 'Polar Squad',
+        url: 'https://polarsquad.com/blog',
+        likes: 1000
+    }
+
+    const component = render(
+        <Blog blog={blog} />
+    )
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    const div = component.container.querySelector('.detail-view')
+    expect(div).not.toHaveStyle('display: none')
+    
+    expect(div).toHaveTextContent(
+        'The best posts about DevOps'
+    )
+
+    expect(div).toHaveTextContent(
+        'Polar Squad'
+    )
+
+    expect(div).toHaveTextContent(
+        'https://polarsquad.com/blog'
+    )
+
+    expect(div).toHaveTextContent(
+        'likes 1000'
     )
 })
