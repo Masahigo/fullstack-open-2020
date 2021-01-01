a85249a09480a1f09d8b9d1c87cc1c79eb98cbdf
Masi Malmi <masi.malmi@polarsquad.com>
2021-01-01 12:44:04 +0200

    Part 5.18

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        const user = {
+            name: 'Masahigo',
+            username: 'higo',
+            password: 'supersalainen'
+        }
+        cy.request('POST', 'http://localhost:3001/api/users/', user) 
         cy.visit('http://localhost:3000')
     })
   
@@ -8,9 +14,28 @@ describe('Blog app', function() {
         cy.get('h2')
             .should('contain', 'Log in to application')
 
-        cy.get('button')
+        cy.get('#login-button')
             .should('contain', 'login')
             .and('have.attr', 'type', 'submit')
 
     })
+
+    describe('Login',function() {
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('higo')
+            cy.get('#password').type('supersalainen')
+            cy.get('#login-button').click()
+    
+            cy.contains('Masahigo logged in')
+        })
+    
+        it('fails with wrong credentials', function() {
+            cy.get('#username').type('higo')
+            cy.get('#password').type('wrong')
+            cy.get('#login-button').click()
+
+            cy.get('html').should('not.contain', 'Masahigo logged in')
+        })
+    }
+    )
   })
