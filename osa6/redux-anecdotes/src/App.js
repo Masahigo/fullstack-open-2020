a2a879270bbfc61c6f289838a007554b18090c96
Masi Malmi <masi.malmi@polarsquad.com>
2021-01-03 11:49:51 +0200

    Part 6.5

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { addVote, createAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotesOrderedByVotes = useSelector(state => state.sort((a,b) => b['votes']-a['votes'])) 
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -21,7 +21,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotesOrderedByVotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
