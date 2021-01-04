f6f851a905bb44bfe28418de81944eace4bc8989
Masi Malmi <masi.malmi@polarsquad.com>
2021-01-04 09:20:41 +0200

    Part 6.20

--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,21 +1,17 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux' 
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = (props) => {
-  const dispatch = useDispatch()
-
+  
   const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     
-    //const newAnecdote = await anecdoteService.createNew(content)
-    //dispatch(createAnecdote(newAnecdote))
-    
-    dispatch(createAnecdote(content))
-    dispatch(setNotification(`New anecdote '${content}' created`, 5))
+    props.createAnecdote(content)
+    props.setNotification(`New anecdote '${content}' created`, 5)
   }
 
   return (
@@ -29,4 +25,12 @@ const AnecdoteForm = (props) => {
   )
 }
 
-export default AnecdoteForm
+const mapDispatchToProps = {
+  createAnecdote,
+  setNotification
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(AnecdoteForm)
--- osa6/redux-anecdotes/src/components/Filter.js
+++ osa6/redux-anecdotes/src/components/Filter.js
@@ -1,24 +1,26 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { filterChange } from '../reducers/filterReducer'
 
-const Filter = () => {
-    const dispatch = useDispatch()
+const Filter = (props) => {
 
-    const handleChange = (event) => {
-      //console.log('filter: ', event.target.value)
-      dispatch(filterChange(event.target.value))
-    }
-
-    const style = {
-      marginBottom: 10
-    }
-  
-    return (
-      <div style={style}>
-        filter <input onChange={handleChange} />
-      </div>
-    )
+  const handleChange = (event) => {
+    //console.log('filter: ', event.target.value)
+    props.filterChange(event.target.value)
   }
-  
-  export default Filter
+
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default connect(
+  null,
+  { filterChange }
+)(Filter)
