e754c60fb2e1fdeb5da498970381a1ab8cc9137d
Masi Malmi <masi.malmi@polarsquad.com>
2021-01-12 10:19:17 +0200

    Part 7.13

--- osa7/bloglist-frontend/src/App.js
+++ osa7/bloglist-frontend/src/App.js
@@ -6,9 +6,11 @@ import Notification from './components/Notification'
 import { setNotification } from './reducers/notificationReducer'
 import { createBlog, removeBlog, addBlogLike, initializeBlogs } from './reducers/blogReducer'
 import { setUser, clearUser } from './reducers/loginReducer'
+import { initializeUsers } from './reducers/userReducer'
 import Togglable from './components/Togglable'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import './App.css'
 
 const App = () => {
   const [username, setUsername] = useState('')
@@ -18,9 +20,14 @@ const App = () => {
   const dispatch = useDispatch()
   const blogs = useSelector(state => state.blogs)
   const user = useSelector(state => state.user)
+  const users = useSelector(state => state.users)
 
   useEffect(() => {
     dispatch(initializeBlogs())
+  }, [dispatch])
+
+  useEffect(() => {
+    dispatch(initializeUsers())
   }, [dispatch])
 
   useEffect(() => {
@@ -138,6 +145,19 @@ const App = () => {
           remove={() => remove(blog)}
         />
       )}
+      <br />
+      <h2>Users</h2>
+      <table>
+          <tbody>
+          <tr><th>&nbsp;</th><th>blogs created</th></tr>
+            {users.map(user => 
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.blogs.length}</td>
+            </tr>
+            )}
+          </tbody>
+      </table>
     </div>
   )
 }
